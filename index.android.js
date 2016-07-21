6c89b62b37c2cb13a1f3b2e96bc0bda3a8eebfbd
zhijieeeeee <tang562250920@gmail.com>
2016-07-21 17:27:00 +0800

    update

--- index.android.js
+++ index.android.js
@@ -11,7 +11,12 @@ import {
   Text,
   View,
   Image,
-  ListView
+  ListView,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  ToastAndroid,
 } from 'react-native';
 
 //本地json测试
@@ -32,10 +37,13 @@ class MovieProject extends Component {
     this.fetchData.bind(this);
   }
 
-  //componentDidMount是React组件的一个生命周期方法，它会在组件刚加载完成的时候调用一次，以后不会再被调用
-  componentDidMount(){
-    //组件加载完成后，调用获取数据
-    this.fetchData();
+  //在ES6语法中生命周期函数getDefaultProps()改为了下面的
+  static defaultProps={
+    value:'我是默认的',
+  };
+  //定义默认属性的类型
+  static propTypes={
+    value:React.PropTypes.string.isRequired,
   }
 
   /** 网络抓取数据 */
@@ -44,6 +52,7 @@ class MovieProject extends Component {
           .then((response) => response.json())
           .then((responseData) => {
             // 在React的工作机制下，setState实际上会触发一次重新渲染的流程
+            // console.log(responseData);
             this.setState({
               // movies: responseData.movies,
               dataSource:this.state.dataSource.cloneWithRows(responseData.movies),
@@ -77,7 +86,6 @@ class MovieProject extends Component {
     )
     // return this.renderMovieView(netMovie);
 
-
     // return (
     //   <View style={styles.container}>
     //     <View>
@@ -93,11 +101,17 @@ class MovieProject extends Component {
     // );
   }
 
+  //componentDidMount是React组件的一个生命周期方法，它会在组件刚加载完成的时候调用一次，以后不会再被调用
+  componentDidMount(){
+    //组件加载完成后，调用获取数据
+    this.fetchData();
+  }
+
   //正在加载视图
   renderLoadingView(){
     return(
       <View style={styles.container}>
-        <Text>正在加载.....</Text>
+        <Text>正在加载.....{this.props.value}</Text>
       </View>
     );
   }
@@ -121,6 +135,68 @@ class MovieProject extends Component {
 
 }
 
+
+class ImageStudyView extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      name:'',
+      count:0,
+    };
+  }
+
+  pressConsole(msg){
+    console.log(msg);
+    ToastAndroid.show(msg, ToastAndroid.SHORT)
+    this.setState({
+      count:this.state.count+1,
+      name:msg,
+    });
+  }
+
+  render(){
+    return(
+      <View style={styles.imageContianer}>
+        {/*调用本地图片*/}
+        <Image source={require('./img/huge.jpg')}/>
+        {/*调用网络图片*/}
+        <Image source={{uri:'http://upload.yoyojie.com/2015/0826/1440558829455.jpg'}}
+              style={{width:100,height:100}}/>
+        <Text>
+          {this.state.name} 点击数：{this.state.count}
+        </Text>
+
+        {/*TouchableHighlight只支持一个子节点*/}
+        <TouchableHighlight onPress={()=>this.pressConsole('TouchableHighlight')} underlayColor={'#ffff00'}
+            style={{backgroundColor:'red'}}>
+            <Text>I'm TouchableHighlight</Text>
+        </TouchableHighlight>
+
+        {/*TouchableNativeFeedback只支持一个单独的View实例作为子节点*/}
+        <TouchableNativeFeedback onPress={this.pressConsole.bind(this,'TouchableNativeFeedback')}
+            background={TouchableNativeFeedback.SelectableBackground()}>
+          <View style={{width: 150, height: 100,backgroundColor:'blue'}}>
+            <Text>I'm TouchableNativeFeedback</Text>
+          </View>
+
+        </TouchableNativeFeedback>
+
+        {/*activeOpacity指定封装的视图在被触摸操作激活时以多少不透明度显示（通常在0到1之间）。*/}
+        <TouchableOpacity onPress={()=>this.pressConsole('TouchableOpacity')} activeOpacity={0.2}>
+            <Text>I'm TouchableOpacity</Text>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+
+  componentDidMount(){
+      console.log('尝试一把');
+      console.log('很好，很不错');
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
     padding:10,
@@ -137,11 +213,16 @@ const styles = StyleSheet.create({
     width:100,
     height:100
   },
+  imageContianer:{
+    flex:1,
+    justifyContent:'space-around',
+    alignItems:'center',
+  },
 });
 
-AppRegistry.registerComponent('MovieProject', () => MovieProject);
+AppRegistry.registerComponent('MovieProject', () => ImageStudyView);
 
-// getDefaultProps
+// getDefaultProps    ES6语法改为使用static defaultProps={};
 // getInitialState
 // componentWillMount
 // render
